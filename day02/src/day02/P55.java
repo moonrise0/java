5a9eb01fa5a6237c1e481e193849725b268e8483
moonrise0 <moonriseisnow@gmail.com>
2023-02-22 17:45:06 +0900

    2023 02 23

---
+++ day02/src/day02/P55.java
@@ -0,0 +1,12 @@
+package day02;
+
+public class P55 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       long a = 2000000000;
+       long b = 2000000000;	   
+		System.out.println(a+b);
+	}
+
+}
