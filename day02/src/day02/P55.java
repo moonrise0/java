528d7ea197443634e3c9348a24655350adf05d68
moonrise0 <moonriseisnow@gmail.com>
2023-02-23 14:12:23 +0900

    "newhomework"

--- day02/src/day02/P55.java
+++ day02/src/day02/P55.java
@@ -4,12 +4,13 @@ public class P55 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       long a = 2000000000;
-       long b = 2000000000;	   
+       int a = 2000000000;
+       int b = 2000000000;	   
        
 //       long result = (a+b);
 //       System.out.println(result);
-		System.out.println(a+b);
+       
+		System.out.println((long)a+b);
 	}
 
 }
