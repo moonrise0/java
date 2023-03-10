3a15b038273f0c2b160c92e9506080360f370b86
moonrise0 <moonriseisnow@gmail.com>
2023-03-10 09:55:55 +0900

    "0309ws"

--- assignment/src/ws0309/Lotto.java
+++ assignment/src/ws0309/Lotto.java
@@ -1,73 +1,63 @@
 package ws0309;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Random;
 
 public class Lotto {
-	
-
-	    private HashSet<Integer> winningNum = new HashSet<Integer>();
-	    private double prizeMoney;
-
-	    public void makeWinningNumberMoney() {
-	        // 당첨 번호 생성
-	        Random rand = new Random();
-	        while (winningNum.size() < 3) {
-	            int num = rand.nextInt(25) + 1;
-	            winningNum.add(num);
-	        }
-	        // 당첨 금액 생성
-	        prizeMoney = rand.nextDouble() * 2000000000;
-	    }
-
-	    public int checkRanking(ArrayList<Integer> myNum) {
-	        // 본인이 입력한 숫자와 당첨 번호 비교
-	        int cnt = 0;
-	        for (int num : myNum) {
-	            if (winningNum.contains(num)) {
-	                cnt++;
-	            }
-	        }
-	        // 등수 리턴
-	        if (cnt == 3) {
-	            return 1;
-	        } else if (cnt == 2) {
-	            return 2;
-	        } else if (cnt == 1) {
-	            return 3;
-	        } else {
-	            return 0;
-	        }
-	    }
-
-	    public double prizeMoney(int grade) {
-	        // 등수에 따른 당첨 금액 비율 설정
-	        if (grade == 1) {
-	            return prizeMoney * 0.7;
-	        } else if (grade == 2) {
-	            return prizeMoney * 0.2;
-	        } else if (grade == 3) {
-	            return prizeMoney * 0.1;
-	        } else {
-	            return 0;
-	        }
-	    }
-
-	    public static void main(String[] args) {
-	        Lotto game = new Lotto();
-	        game.makeWinningNumberMoney();
-
-	        // 본인이 입력한 숫자
-	        ArrayList<Integer> myNum = new ArrayList<Integer>(Arrays.asList(1, 2, 3));
-
-	        // 등수와 당첨 금액 출력
-	        int grade = game.checkRanking(myNum);
-	        double prize = game.prizeMoney(grade);
-	        System.out.println("당첨 번호: " + game.winningNum);
-	        System.out.println("내 번호: " + myNum);
-	        System.out.println("등수: " + grade);
-	        System.out.println("당첨 금액: " + prize);
-	    }
-	}
+    
+    private HashSet<Integer> winningNum;
+    private double prizeMoney;
+    
+    // 생성자
+    public Lotto() {
+        this.winningNum = new HashSet<Integer>();
+        this.prizeMoney = 0.0;
+    }
+    
+    // 당첨 번호와 당첨 금액을 생성하는 함수
+    public void makeWinningNumberMoney() {
+        Random random = new Random();
+        while (winningNum.size() < 3) {
+            int num = random.nextInt(25) + 1;
+            winningNum.add(num);
+        }
+        prizeMoney = random.nextDouble() * 2000000000;
+    }
+    
+    // 내가 선택한 숫자와 당첨 번호를 비교하여 등수를 반환하는 함수
+    public int checkRanking(ArrayList<Integer> myNum) {
+        int matchCount = 0;
+        for (int num : myNum) {
+            if (winningNum.contains(num)) {
+                matchCount++;
+            }
+        }
+        // 등수는 자유롭게 설정 가능
+        if (matchCount == 3) {
+            return 1;
+        } else if (matchCount == 2) {
+            return 2;
+        } else if (matchCount == 1) {
+            return 3;
+        } else {
+            return -1; // 꽝
+        }
+    }
+    
+    // 등수 정보를 입력하면 당첨 금액을 반환하는 함수
+    public double prizeMoney(int grade) {
+        // 등수에 따른 당첨금 비율은 자유롭게 설정 가능
+        switch (grade) {
+            case 1:
+                return prizeMoney * 0.7;
+            case 2:
+                return prizeMoney * 0.2;
+            case 3:
+                return prizeMoney * 0.1;
+            default:
+                return 0.0; // 꽝
+        }
+    }
+    
+}
