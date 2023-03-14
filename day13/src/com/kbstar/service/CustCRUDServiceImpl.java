9b478e78cdfe2fbfe01e1c7a698106ad67566cba
moonrise0 <moonriseisnow@gmail.com>
2023-03-14 10:31:40 +0900

    "0314"

--- day13/src/com/kbstar/service/CustCRUDServiceImpl.java
+++ day13/src/com/kbstar/service/CustCRUDServiceImpl.java
@@ -9,73 +9,86 @@ import com.kbstar.dto.Cust;
 import com.kbstar.frame.CRUDService;
 import com.kbstar.frame.DAO;
 
-public class CustCRUDServiceImpl implements CRUDService <String,Cust> {
+public class CustCRUDServiceImpl implements CRUDService<String, Cust> {
 
 	DAO<String, String, Cust> dao;
-	
+
 	public CustCRUDServiceImpl() {
-	   dao = new CustDaoImpl();
+		dao = new CustDaoImpl();
 	}
-	
+
 	@Override
 	public void register(Cust v) throws Exception {
 		// 데이터 검증
 		// DB 입력
-		
+
 		try {
-		dao.insert(v);
-		}catch(Exception e) {
-			if(e instanceof SQLIntegrityConstraintViolationException) {
-			throw new Exception("ID가 중복되었습니다.");
-		}else {
-			throw new Exception("시스템장애입니다.");
-		}}
-		
-		
+			dao.insert(v);
+		} catch (Exception e) {
+			if (e instanceof SQLIntegrityConstraintViolationException) {
+				throw new Exception("ID가 중복되었습니다.");
+			} else {
+				throw new Exception("시스템장애입니다.");
+			}
+		}
+
 	}
 
 	@Override
 	public void modify(Cust v) throws Exception {
 		try {
 			dao.update(v);
-		}catch(Exception e) {
-			
+		} catch (Exception e) {
+
 		}
-		
-		
+
 	}
 
 	@Override
 	public void remove(String k) throws Exception {
 		try {
 			dao.delete(k);
-		}catch(Exception e) {
-			
-			if(e instanceof SQLRecoverableException) {
+		} catch (Exception e) {
+
+			if (e instanceof SQLRecoverableException) {
 				throw new Exception("시스템 장애");
-			}else {
+			} else {
 				throw new Exception("id 장애");
 			}
 		}
-		
-		
+
 	}
 
 	@Override
 	public Cust get(String k) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		Cust cust = null;
+		try {
+		cust = dao.select(k);
+		}catch(Exception e) {
+			if(e instanceof SQLRecoverableException) {
+				throw new Exception("시스템 장애입니다.");
+			}else {
+				throw new Exception("ID가 존재 하지 않습니다");
+			}
+		}
+		return cust;
 	}
 
 	@Override
 	public List<Cust> get() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	
-	
+		List<Cust> list = null;
 		
+	    try {
+		list = dao.selectAll();   //이거 안해주면 화면에 이상한 거 뜸.
+	    }catch(Exception e) {
+	    	if(e instanceof SQLRecoverableException) {
+				throw new Exception("시스템 장애입니다.");
+			}else {
+				throw new Exception("ID가 존재 하지 않습니다");
+			}
+	    }
+		return list;
 	}
 
-
+}
--- day13/src/jdbc/CustSelectAllTest.java
+++ day13/src/jdbc/CustSelectAllTest.java
@@ -1,19 +1,27 @@
 package jdbc;
 
+import java.util.List;
+
 import com.kbstar.dto.Cust;
 import com.kbstar.frame.CRUDService;
 import com.kbstar.service.CustCRUDServiceImpl;
 
-public class CustInsertTest {
+public class CustSelectAllTest {
 
 	public static void main(String[] args) {
 		CRUDService<String, Cust> crudService =
 				new CustCRUDServiceImpl();
-		
-		Cust cust = new Cust("id03", "pwd03", "tom", 21);
+		List<Cust> list = null;
 		try {
-			crudService.register(cust);
-			System.out.println("성공");
+			list = crudService.get();
+			if(list.size()== 0) {
+				System.out.println("데이터가 없습니다.");
+				return;  //리턴의 의미는 더이상  for 문 하지 말라.
+			}
+			
+			for(Cust obj: list) {
+				System.out.println(obj);
+			}
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
