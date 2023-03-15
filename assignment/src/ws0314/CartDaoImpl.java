f8ebac3df20e419258db67fddcbc1513201d5dab
moonrise0 <moonriseisnow@gmail.com>
2023-03-15 12:54:48 +0900

    "ws0314"

--- assignment/src/ws0314/CartDaoImpl.java
+++ assignment/src/ws0314/CartDaoImpl.java
@@ -106,32 +106,38 @@ public class CartDaoImpl implements DAO<String, String, Cart> {
 		}
 		return cartList;
 	}
+//
+//	@Override
+//	public List<Cart> search(String k) throws Exception {
+//		
+//		List<Cart> cartList = new ArrayList<>();
+//		try (Connection con = getConnection(); PreparedStatement pstmt = con.prepareStatement(Sql.mycartSelectAllSql))
+//		
+//		{   pstmt.setString(1, k);
+//		
+//			try (ResultSet rset = pstmt.executeQuery()) {
+//				while (rset.next()) {
+//					String id = rset.getString("id");
+//					String userId = rset.getString("user_id");
+//					String itemId = rset.getString("item_id");
+//					int cnt = rset.getInt("cnt");
+//					Date regdate = rset.getDate("regdate");
+//					cartList.add(new Cart(id, userId, itemId, cnt, regdate));
+//				}
+//				if (cartList.size() == 0) {
+//					throw new Exception("카트가 빈 상태입니다!");
+//				}
+//			}
+//		} catch (SQLException e) {
+//			throw e;
+//		}
+//		return cartList;
+//	}
 
 	@Override
 	public List<Cart> search(String k) throws Exception {
-		
-		List<Cart> cartList = new ArrayList<>();
-		try (Connection con = getConnection(); PreparedStatement pstmt = con.prepareStatement(Sql.mycartSelectAllSql))
-		
-		{   pstmt.setString(1, k);
-		
-			try (ResultSet rset = pstmt.executeQuery()) {
-				while (rset.next()) {
-					String id = rset.getString("id");
-					String userId = rset.getString("user_id");
-					String itemId = rset.getString("item_id");
-					int cnt = rset.getInt("cnt");
-					Date regdate = rset.getDate("regdate");
-					cartList.add(new Cart(id, userId, itemId, cnt, regdate));
-				}
-				if (cartList.size() == 0) {
-					throw new Exception("카트가 빈 상태입니다!");
-				}
-			}
-		} catch (SQLException e) {
-			throw e;
-		}
-		return cartList;
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	}
